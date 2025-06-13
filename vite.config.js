ca5c701d408f90b2285ec70e7e85fdb37c94e067
marchelxyz <92934534+marchelxyz@users.noreply.github.com>
2025-06-13 23:16:55 +0300

    Update vite.config.js
    Fix Vite root config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
+  root: '.', // указывает, что index.html лежит в корне проекта
   build: {
     outDir: 'dist'
   }
